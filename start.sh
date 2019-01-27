04ced64adf2a470421a47298c17ba6d915647a47
iamzubin <zchoudhary.10@gmail.com>
2019-01-27 23:57:02 +0530

    initital script

---
+++ start.sh
@@ -0,0 +1,79 @@
+#install pip
+sudo apt install python-pip -y
+
+#install curl
+sudo apt-get install curl -y
+
+
+#install build-essentials
+
+sudo apt-get install -y build-essential libssl-dev
+
+#install Git
+sudo apt-get install -y git xclip
+
+# install snap
+sudo apt-get install -y snap
+
+#installing google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+#install ZSH
+sudo apt-get -y install zsh
+chsh -s /bin/zsh
+
+
+
+# Nodejs and NVM
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+source ~/.profile
+sudo nvm install 7.10.0
+sudo nvm use 7.10.0
+node -v
+
+#install npm
+sudo apt-get install -y npm
+
+#nodemon
+sudo npm install -g nodemon
+sudo npm install -g loopback-cli
+
+
+# Gulp - an automated task runner
+sudo npm install -g gulp-cli
+
+# FileZilla - a FTP client
+sudo apt-get install -y filezilla
+
+
+# Sticky Notes
+sudo apt-get install -y xpad
+
+
+#install VS-code
+sudo snap install vscode --classic
+
+#install spotify
+
+sudo snap install spotify
+
+#install cheat
+
+sudo snap install cheat
+
+#gnome tweak tool
+
+sudo apt-get install gnome-tweak-tool
+sudo apt-get install chrome-gnome-shell
+
+
+#installing discord
+sudo snap install discord
+
+
+# howdy
+
+sudo add-apt-repository ppa:boltgolt/howdy
+sudo apt update
+sudo apt install howdy
