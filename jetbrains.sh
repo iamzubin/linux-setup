d7c74ae501a6992a88789c5f2bb857de785d2389
Shivansh Yadav <shivanshyadav00@gmail.com>
2019-10-27 18:17:12 +0530

    Added additional script to install jetbrains products

---
+++ jetbrains.sh
@@ -0,0 +1,23 @@
+#install Webstorm
+sudo snap install webstorm --classic
+
+#install PhpStorm
+sudo snap install phpstorm --classic
+
+#install DataGrip
+sudo snap install datagrip --classic
+
+#install GoLand
+sudo snap install goland --classic
+
+#install CLion
+sudo snap install clion --classic
+
+#install IntelliJ
+sudo snap install intellij-idea-ultimate --classic
+
+#install Pycharm
+sudo snap install pycharm-professional --classic
+
+#install RubyMine
+sudo snap install rubymine --classic
