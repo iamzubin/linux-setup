f0cfc38a07cfc1f82f1f4a94b6728de59c7bd41b
zchoudhary.10@gmail.com <zchoudhary.10@gmail.com>
2021-05-11 02:14:27 +0530

    manjaro

---
+++ openbox/autostart.sh
@@ -0,0 +1,13 @@
+pasystray &
+nm-tray &
+polybar mybar &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+nitrogen --restore; sleep 1; picom -b &
+nm-applet &
+xfce4-power-manager &
+pamac-tray &
+clipit &
+blueman-applet &
+start_conky_green &
+ff-theme-util &
+fix_xcursor &
